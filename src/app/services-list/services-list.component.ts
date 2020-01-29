a2e82d2b3f790a40b8986d327e95e00cead791d6
Nick Thesing <nickthesing@gmail.com>
2020-01-29 15:42:26 +0100

    Functionality: design for services list

--- src/app/services-list/services-list.component.ts
+++ src/app/services-list/services-list.component.ts
@@ -12,98 +12,42 @@ import { Service } from '../service/service.interface';
 import {FlatTreeControl} from '@angular/cdk/tree';
 import {MatTreeFlatDataSource, MatTreeFlattener} from '@angular/material/tree';
 
-/**
- * Food data with nested structure.
- * Each node has a name and an optiona list of children.
- */
-interface FoodNode {
-  name: string;
-  children?: FoodNode[];
-}
-
-const TREE_DATA: FoodNode[] = [
-  {
-    name: 'Fruit',
-    children: [
-      {name: 'Apple'},
-      {name: 'Banana'},
-      {name: 'Fruit loops'},
-    ]
-  }, {
-    name: 'Vegetables',
-    children: [
-      {
-        name: 'Green',
-        children: [
-          {name: 'Broccoli'},
-          {name: 'Brussel sprouts'},
-        ]
-      }, {
-        name: 'Orange',
-        children: [
-          {name: 'Pumpkins'},
-          {name: 'Carrots'},
-        ]
-      },
-    ]
-  },
-];
-
-/** Flat node with expandable and level information */
-interface ExampleFlatNode {
-  expandable: boolean;
-  name: string;
-  level: number;
-}
-
 @Component({
   selector: 'app-services-list',
   templateUrl: './services-list.component.html',
   styleUrls: ['./services-list.component.scss']
 })
 export class ServicesListComponent implements OnInit {
-
-  private _transformer = (node: FoodNode, level: number) => {
-    return {
-      expandable: !!node.children && node.children.length > 0,
-      name: node.name,
-      level: level,
-    };
-  }
-
-  treeControl = new FlatTreeControl<ExampleFlatNode>(
-      node => node.level, node => node.expandable);
-
-  treeFlattener = new MatTreeFlattener(
-      this._transformer, node => node.level, node => node.expandable, node => node.children);
-
-  dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
-
-  hasChild = (_: number, node: ExampleFlatNode) => node.expandable;
-
   readonly services$: Observable<any> = this.store.pipe(select(servicesSelectors.getServices));
-
-
   deleteService$: Observable<any>;
+  allOpen: boolean = false;
 
   constructor(
     private store: Store<any>,
     private homeService: HomeService,
     private toastr: ToastrService,
-    private router: Router) {
-
-      this.dataSource.data = TREE_DATA;
-    }
+    private router: Router) {}
 
   ngOnInit() {
     this.store.dispatch(new servicesActions.LoadServices());
+  }
+
+  customFormat(endpoint: string, serviceName: string): string {
+    return endpoint
+      .replace('/gateway/api', '')
+      .replace(serviceName, '')
+      .replace('//client-api/v2', '');
+  }
+
+  toggleAll(services: any[]) {
+    this.allOpen = !this.allOpen;
+    services.map(service => service.open = this.allOpen);
   }
 
   deleteService(serviceName: string) {
     this.deleteService$ = this.homeService.deleteService(serviceName).pipe(
       tap(() => {
         this.store.dispatch(new servicesActions.DeleteServiceSuccess(serviceName));
-
         this.toastr.success('Deleted successfully!', serviceName);
       }),
       catchError ((errorResponse) => {
@@ -113,10 +57,4 @@ export class ServicesListComponent implements OnInit {
       })
     );
   }
-
-  setSelectedService(service: Service) {
-    this.store.dispatch(new servicesActions.SetSelectedService(service));
-    this.router.navigate(['/services', service.serviceName]);
-  }
-
 }
