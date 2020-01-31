1b10daab76718070c44fa7d6a16cfd16831d46ad
Kayzhe Tsar <kayzhe@sozeh.com>
2020-01-31 11:39:57 +0100

    Add Control Value Accessor on the drop down

--- src/app/dropdown/dropdown.component.ts
+++ src/app/dropdown/dropdown.component.ts
@@ -1,18 +1,25 @@
-import { Component, OnInit, Input, Output, EventEmitter, ChangeDetectionStrategy, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ChangeDetectionStrategy, OnChanges, SimpleChanges, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-dropdown',
   templateUrl: './dropdown.component.html',
   styleUrls: ['./dropdown.component.scss'],
   changeDetection: ChangeDetectionStrategy.Default,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => DropdownComponent),
+      multi: true
+    }
+  ]
 })
-export class DropdownComponent implements OnInit, OnChanges {
+export class DropdownComponent implements OnInit, OnChanges, ControlValueAccessor {
   @Input() items: any[] = [];
   @Input() defaultValue: string;
   @Input() defaultHeader: string;
 
   @Output() setSelectedValue: EventEmitter<string> = new EventEmitter<string>();
-
   selectedValue: string;
   isOpen = false;
 
@@ -28,9 +35,26 @@ export class DropdownComponent implements OnInit, OnChanges {
     this.selectedValue = this.defaultValue || this.defaultHeader;
   }
 
+
+  onChanged: any = () => {}
+  onTouched: any = () => {}
+
+  writeValue(val) {
+    this.selectedValue = val;
+  }
+
+  registerOnChange(fn: any){
+    this.onChanged = fn
+  }
+  registerOnTouched(fn: any){
+    this.onTouched = fn
+  }
+
   setSelectedValueOnParent(item: string): void {
     this.selectedValue = item;
     this.setSelectedValue.emit(item);
+    this.onChanged(item);
+    this.onTouched();
   }
 
   toggleDropdown($event: any): void {
