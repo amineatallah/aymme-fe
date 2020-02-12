e387f4b15253aea78a7d20b0823b1c96404b54a9
Nick Thesing <nickthesing@gmail.com>
2020-02-12 14:20:20 -0500

    #AYM-9 - Implement sticky header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
+import { fromEvent, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, AfterViewInit {
+  isScrollingDown = false;
+  scroll$: Observable<any>;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit(): void {
+    this.scroll$ = fromEvent(window, 'scroll').pipe(
+      map(() => window.pageYOffset)
+    );
+
+    this.scroll$.subscribe(res => {
+      this.isScrollingDown = res >= 80 ? true : false;
+    });
   }
 
 }
