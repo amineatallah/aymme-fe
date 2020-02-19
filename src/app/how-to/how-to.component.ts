7ee352e3a5f3b473d6b61c1b6606e631c35685be
Kayzhe Tsar <kayzhe@hotmail.com>
2020-02-19 16:27:38 +0100

    Add description of AYMME functionality and how to Mock Experience Model

--- src/app/how-to/how-to.component.ts
+++ src/app/how-to/how-to.component.ts
@@ -26,6 +26,75 @@ export class HowToComponent implements OnInit {
 
   public disableMockCode = `localStorage.setItem("enableMocks", false)`;
 
+  public environmentCode = `
+  import { Environment } from './type';
+  import { ExternalServices } from '@backbase/foundation-ang/start';
+
+  const services: ExternalServices = {};
+
+  export const environment: Environment = {
+      production: false,
+  };
+
+  let experienceName = 'your-experience-name';
+
+  fetch('http://localhost:3000/api/simpleModel/' + experienceName)
+      .then(
+          function (response) {
+              if (response.status !== 200) {
+                  console.log('Unexpected error: Unable to retrieve Experience Model. Status Code: ' +
+                      response.status);
+                  return;
+              }
+              // Examine the text in the response
+              response.json().then(function (data) {
+                  window.BB.startSingleApp(services).then((app: any) => app.bootstrap(data.children[0]));
+              });
+          }
+      )
+      .catch(function (err) {
+          console.log('Fetch Error: Unable to retrieve Experience Model', err);
+      });`
+
+  public angularJSONcode = `{
+        "$schema": "./node_modules/@angular/cli/lib/config/schema.json",
+        "version": 1,
+        "newProjectRoot": "",
+        "projects": {
+          "banking-app": {
+            //...
+            "architect": {
+              //...
+              "build": {
+                "configurations": {
+                  //... ADD THE PROPERTY BELOW
+                  "aymme": {
+                    "fileReplacements": [
+                      {
+                        "replace": "apps/your-app-name/src/environments/environment.ts",
+                        "with": "apps/your-app-name/src/environments/environment.aymme.ts"
+                      }
+                    ]
+                  },
+                }
+              },
+              "serve": {
+                //...
+                "configurations": {
+                  //... ADD THE PROPERTY BELOW
+                  "aymme": {
+                    "browserTarget": "your-app-name:build:aymme"
+                  }
+                }
+              }
+            }
+          }
+        }
+      }`
+
+
+  public simplifiedModelScriptsCode = `"start:aymme": "ng serve --proxy-config=proxy.conf.aymme.json --configuration=aymme"`;
+
   constructor() { }
 
   ngOnInit() {
